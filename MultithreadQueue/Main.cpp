2db5470bf0daf7bada290e4c135d5dd61943c095
OAI <lskomorohl@gmail.com>
2019-01-30 16:23:21 +0200

    Final

--- MultithreadQueue/Main.cpp
+++ MultithreadQueue/Main.cpp
@@ -8,10 +8,11 @@ int main()
 	size_t queueMaxSize = 10'000;
 	size_t firstThread = queueMaxSize - 3'000;
 	size_t secondThread = queueMaxSize - firstThread;
+	size_t threadCount = 10;
 	int resSum = 0;
 	std::atomic<int> sum = 0;
-	int checkSum = queueMaxSize / 2 * (queueMaxSize - 1);
-	std::thread th([&]()
+	int checkSum = threadCount * (queueMaxSize / 20 * (queueMaxSize / 10 - 1));
+	std::thread th1([&]()
 	{
 		for (int i = 0; i < firstThread; i++)
 		{
@@ -19,23 +20,50 @@ int main()
 		}
 	});
 
-	for (int i = 0; i < 10; i++)
+	std::thread th2([&]()
+	{
+		for (int i = 0; i < secondThread; i++)
+		{
+			sum += tQueue.Pop();
+		}
+	});
+
+	for (int i = 0; i < threadCount; i++)
 	{
 		tVec.push_back(std::thread([&]() {
 			for (int j = 0; j < 1'000; ++j)
 				tQueue.Push(j);
 		}));
 	}
-	for (int i = 0; i < secondThread; i++)
-	{
-		sum += tQueue.Pop();
-	}
-	for (int i = 0; i < 10; i++)
+	
+	for (int i = 0; i < threadCount; i++)
 	{
 		tVec[i].join();
 	}
 
-	th.join();
+	char flag = ' ';
+	std::cout << "Press q to fin... \n";
+	std::cin >> flag;
+
+	if (flag == 'q')
+	{
+		th1.detach();
+		th2.detach();
+	}
+	else
+	{
+		th1.join();
+		th2.join();
+	}
+	
 	resSum = sum;
+	if (resSum == checkSum)
+	{
+		std::cout << "Sucsess!";
+	}
+	else
+	{
+		std::cout << "Fail!";
+	}
 	return 0;
 }
